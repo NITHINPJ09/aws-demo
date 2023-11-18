2fbb129df8c9d9da0bfbfa067ac0f6bd4b916d35
Nithin Prince John <nithinpj09@gmail.com>
2023-11-18 18:54:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,9 @@ resource "random_pet" "lambda_bucket_name" {
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket        = random_pet.lambda_bucket_name.id
   force_destroy = true
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
